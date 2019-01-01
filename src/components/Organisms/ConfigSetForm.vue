ea899eebdd8822894cc2249d5aa4123b157260d8
naporitan <naporin0624@gmail.com>
2019-01-01 15:32:04 +0900

    Revert "Revert "コンポーネント切り分け途中""
    This reverts commit 9bab90785cf419703a43e7d33cbc51932a82b3b7.

---
+++ src/components/Organisms/ConfigSetForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <fieldset>
+    <legend>反応ボイスを入力してね</legend>
+    <inputform-component
+      v-bind:input.sync="localConfig.form.input"
+      v-bind:title.sync="localConfig.form.title"
+      v-bind:audio.sync="localConfig.form.audio"
+      v-bind:selector="localConfig.selector"
+    ></inputform-component>
+    <div>
+      <button @click="pushEvent" v-bind:disabled="canPushAddButton">この設定で追加</button>
+      <button @click="trialPlayAudio" v-bind:disabled="canPushAddButton">{{audioMessage}}</button>
+    </div>
+  </fieldset>
+</template>
+
+<script>
+import InputForm from "../Molecules/InputForm";
+export default {
+  name: "ConfigSetForm",
+  components: {
+    "inputform-component": InputForm
+  },
+  props: {
+    config: Object
+  },
+  data() {
+    return {
+      audioMessage: ""
+    };
+  },
+  computed: {
+    localConfig: {
+      get() {
+        return this.config;
+      },
+      set(config) {
+        this.$emit("update:config", config);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
